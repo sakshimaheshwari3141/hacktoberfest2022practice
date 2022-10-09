00f5a02417afed06fcb212a0e678880911bd00e3
parakh2204 <parakh2204@gmail.com>
2022-10-09 20:43:00 +0530

    Update invertCase.java

--- invertCase.java
+++ invertCase.java
@@ -1,6 +1,6 @@
 public class invertCase {
     public static void main(String[] args) {
-        //UpperCae to LowerCase
+        //UpperCase to LowerCase
         System.out.println((char)('N'^' '));
         System.out.println((char)('a'^' '));
     }
