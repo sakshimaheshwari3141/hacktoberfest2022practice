a1c8db3cb9609f17a652e72dba4cc21a3b02119d
Sakshi Maheshwari <56722443+sakshimaheshwari3141@users.noreply.github.com>
2023-10-29 23:00:10 +0530

    bit manipulation

---
+++ SwapTwoNumber.java
@@ -0,0 +1,11 @@
+public class SwapTwoNumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        System.out.println(a+" "+b);
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        System.out.println(a+" "+b);
+    }
+}
