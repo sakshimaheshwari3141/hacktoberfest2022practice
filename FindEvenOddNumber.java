af835e278518a8a9f58cc78682bcbb7a28ebe8e8
Sakshi Maheshwari <56722443+sakshimaheshwari3141@users.noreply.github.com>
2023-10-29 22:56:20 +0530

    a new program created

---
+++ FindEvenOddNumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class FindEvenOddNumber {
+    public static void main(String[] args) {
+        int N = 13;
+        if((N&1)==1) {
+            System.out.println("Odd Number");
+        }
+        else {
+            System.out.println("Even Number");
+        }
+    }
+}
